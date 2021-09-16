25056c3f1e5954af4058e671fcd16a1008e2457b
aixinaihongjun <1181391196@qq.com>
2021-09-16 12:52:27 +0800

    修改mixin.js

--- aixinaihongjun_website_pc/src/utils/mixin.js
+++ aixinaihongjun_website_pc/src/utils/mixin.js
@@ -1,9 +1,17 @@
 import config from "@/config";
 
 export default {
+    data() {
+        return {};
+    },
+    computed: {
+        baseUrl() {
+            return config.baseUrl;
+        }
+    },
     methods: {
         getImgUrl(image) {
             return config.baseUrl + image;
-        },
-    },
-}
+        }
+    }
+};
